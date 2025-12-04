4d803e41d4595330585b08af386407e2363e6006
Kai <kai@gem.health>
2025-12-04 17:57:37 -0500

    tiny and petit net

--- cs440/src/pas/pokemon/agents/PolicyAgent.java
+++ cs440/src/pas/pokemon/agents/PolicyAgent.java
@@ -94,14 +94,11 @@ public class PolicyAgent
         // currently this creates a one-hidden-layer network
         Sequential qFunction = new Sequential();
         // -------------------layer 1----------------------
-        qFunction.add(new Dense(13, 52)); // number of input features , number of neurons in this hidden layer
+        qFunction.add(new Dense(13, 13)); // number of input features , number of neurons in this hidden layer
         qFunction.add(new Tanh()); // non-linear activation function
         // ------------------end of layer 1----------------
         // -------------------layer 2----------------------
-        qFunction.add(new Dense(52, 104)); // number of input features , number of neurons in this hidden layer
-        qFunction.add(new Tanh());
-        // -------------------layer 2----------------------
-        qFunction.add(new Dense(104, 1)); // number of input features , number of neurons in this hidden layer
+        qFunction.add(new Dense(13, 1)); // number of input features , number of neurons in this hidden layer
         return qFunction;
     }
 
