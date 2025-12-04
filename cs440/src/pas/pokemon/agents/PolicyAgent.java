6eb90125cbe1132efe8d5fea21660d949c54a7c9
Becky Geisberg <rgeis26@bu.edu>
2025-12-04 17:38:51 -0500

    new branch

--- cs440/src/pas/pokemon/agents/PolicyAgent.java
+++ cs440/src/pas/pokemon/agents/PolicyAgent.java
@@ -97,10 +97,6 @@ public class PolicyAgent
         qFunction.add(new Dense(128, 512)); // number of input features , number of neurons in this hidden layer
         qFunction.add(new Tanh()); // non-linear activation function
         // ------------------end of layer 1----------------
-        // -------------------layer 2----------------------
-        qFunction.add(new Dense(512, 512)); // number of input features , number of neurons in this hidden layer
-        qFunction.add(new Tanh()); // non-linear activation function
-        // ------------------end of layer 2----------------
         // ------------------Final Layer-------------------
         qFunction.add(new Dense(512, 1)); // final dense layer, maps to 1
         return qFunction;
