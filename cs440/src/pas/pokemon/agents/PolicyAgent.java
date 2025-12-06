276ae9d189864683a73addbfd2b1d0f9419ffcdd
Becky Geisberg <rgeis26@bu.edu>
2025-12-06 14:49:04 -0500

    new neural net archetecture

--- cs440/src/pas/pokemon/agents/PolicyAgent.java
+++ cs440/src/pas/pokemon/agents/PolicyAgent.java
@@ -98,15 +98,15 @@ public class PolicyAgent
         qFunction.add(new Tanh()); // non-linear activation function
         // ------------------end of layer 1----------------
         // -------------------layer 2----------------------
-        qFunction.add(new Dense(512, 512)); // number of input features , number of neurons in this hidden layer
+        qFunction.add(new Dense(512, 1024)); // number of input features , number of neurons in this hidden layer
         qFunction.add(new Tanh()); // non-linear activation function
         // ------------------end of layer 2----------------
         // -------------------layer 3----------------------
-        qFunction.add(new Dense(512, 256)); // number of input features , number of neurons in this hidden layer
+        qFunction.add(new Dense(1024, 2048)); // number of input features , number of neurons in this hidden layer
         qFunction.add(new Tanh()); // non-linear activation function
         // ------------------end of layer 3----------------
         // ------------------Final Layer-------------------
-        qFunction.add(new Dense(256, 1)); // final dense layer, maps to 1
+        qFunction.add(new Dense(2048, 1)); // final dense layer, maps to 1
         return qFunction;
     }
 
