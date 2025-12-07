9a9e3a6d60e1b80b249b4dc3dff468cd9e4dda59
Becky Geisberg <rgeis26@bu.edu>
2025-12-07 15:09:38 -0500

    implemented penalty for turns

--- cs440/src/pas/pokemon/rewards/CustomRewardFunction.java
+++ cs440/src/pas/pokemon/rewards/CustomRewardFunction.java
@@ -7,6 +7,7 @@ import edu.bu.pas.pokemon.agents.rewards.RewardFunction;
 import edu.bu.pas.pokemon.agents.rewards.RewardFunction.RewardType;
 import edu.bu.pas.pokemon.core.Pokemon;
 import edu.bu.pas.pokemon.core.Battle.BattleView;
+import edu.bu.pas.pokemon.core.Battle;
 import edu.bu.pas.pokemon.core.Move.MoveView;
 import edu.bu.pas.pokemon.core.Pokemon.PokemonView;
 import edu.bu.pas.pokemon.core.Team.TeamView;
@@ -168,6 +169,12 @@ public class CustomRewardFunction
         // positive if we expect to deal more damage than we take
         double damageReward = oppHPDamageExp - myHPDamageExp;
 
+        // penalize battle steps;
+        Battle battle = new Battle(state);
+        int numTurns = battle.getTurnNumber();
+
+        double penalty = -0.05 * numTurns;
+
         damageReward = Math.max(-200.0, Math.min(200.0, damageReward));
         koReward = Math.max(-1.0, Math.min(1.0, koReward));
         teamHPReward = Math.max(-2.0, Math.min(2.0, teamHPReward));
@@ -177,7 +184,7 @@ public class CustomRewardFunction
         if (winnerReward != 0) {
             reward = winnerReward;
         } else {
-            reward = 0.25 * damageReward + 30 * koReward;
+            reward = 0.25 * damageReward + 30 * koReward + penalty;
         }
 
         reward = Math.max(-100.0, Math.min(100.0, reward));
